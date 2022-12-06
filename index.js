d5f00ac69cf48d7f4594f056c0e5beb418b76cfe
Karthik Iyer <karthik.iyer@clevertap.com>
2022-12-06 13:48:24 +0530

    twelfth commit

--- index.js
+++ index.js
@@ -1,7 +1,6 @@
 const express = require("express");
 const app = express();
 const importData = require("./data.json");
-const importData2 = require("./data1.json");
 let port = process.env.PORT || 3000;
 
 app.get("/", (req, res) => {
@@ -10,10 +9,6 @@ app.get("/", (req, res) => {
 
 app.get("/cartdetails", (req, res) => {
   res.send(importData);
-});
-
-app.get("/cartdetails?id=kk1611", (req, res) => {
-  res.send(importData2);
 });
 
 app.listen(port, () => {
